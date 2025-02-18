76080de0ddeffc04dbd217c49186454257a6e3de
Shreya <shreya16180@gmail.com>
2025-02-18 23:46:11 +0530

    App.jsx, Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Projects from "../pages/Projects";
+// import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Work from "./components/Work";
 
 function App() {
 	return (
-		<Routes>
-			<Route
-				path="/"
-				element={<Home />}
-			/>
-			<Route
-				path="/projects"
-				element={<Projects />}
-			/>
-		</Routes>
+		<>
+			<div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+			</div>
+			<main className="flex flex-col items-center px-4 mg:px-8 lg:px-16">
+				<Navbar></Navbar>
+				<Hero></Hero>
+				<About></About>
+				<Contact></Contact>
+				<Work></Work>
+			</main>
+		</>
 	);
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+	return (
+		<nav className="z-10 fixed top-0 flex flex-row w-full items-center justify-between border-b border-b-gray-700 bg-black/70 px-16 py-6 text-white backdrop-blur-md md:justify-evenly">
+      <a href="">Shreya</a>
+    </nav>
+	);
+};
+
+export default Navbar;
